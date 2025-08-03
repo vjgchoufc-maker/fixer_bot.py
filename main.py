07c4d4e2f8b80ca750e1f13d11a1b93b6f875534
vjgchoufc-maker <vjgchoufc@gmail.com>
2025-08-03 06:10:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from telegram.ext import (
 import traceback  
   
 # استبدل هذا بـ API Token الخاص بروبوتك  
-TOKEN =  "8293785720:AAGZCRwR3_r93E-yd8S04Q0PmowCX-OPe0k"  
+TOKEN =  ""  
   
 def fix_python_code(code_string):  
     """  
